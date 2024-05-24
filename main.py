6c77b8af4f200bcbe262e37e656c44abd1940e1b
alex <forrmaldehyde@protonmail.com>
2024-05-24 15:15:56 +0300

    hello worlsdladoska

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from gui import root
 from CTkMessagebox import CTkMessagebox
 
 if __name__ == '__main__':
+    os.open('run.bat')
     CTkMessagebox(title='Сообщение', message='Открыть файл ReadMe?', icon='question',
                   option_1='Да', option_2='Нет', option_3='Да')
     root.mainloop()
--- run.bat
+++ run.bat
@@ -1,5 +1,2 @@
 pip3 install -r requirements.txt
-python main.py
-
-pip install -r requirements.txt
-python main.py
+exit
